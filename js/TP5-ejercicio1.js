f9ca2f67274b538620047904fc0b9b67172536a1
Azul De Pol <azul.depol@gmail.com>
2023-04-17 20:52:45 +0100

    trabaje la funcion send de acuerdo al enunciado

--- js/TP5-ejercicio1.js
+++ js/TP5-ejercicio1.js
@@ -1,43 +1,40 @@
 /*Números mágicos - Dificultad:  🟢
 1- Crea una web con bootstrap y js, que contenga un botón comenzar el juego, en ese momento se crea un número aleatorio que el usuario deberá adivinar, la interfaz del usuario debe tener además un input para ingresar un número y un botón enviar, al presionar el botón enviar mostrar en un alert si el usuario adivino o no el número mágico, si no lo adivino indicarle con un alert si el numero que ingreso es mayor o menor al número mágico.
 Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero.
- */
+*/
 
 let botonComenzar = document.getElementById("start");
 let formulario = document.querySelector("form");
 let magicNumber = [];
 let userGuess = [];
 let displayResult = document.getElementById("displayResult");
+let adivino = (magicNumber === userGuess) ? "Adivinaste!" : "No adivinaste!"; 
 console.log(formulario);
 
-formulario.addEventListener("submit", obtenerDato);
+formulario.addEventListener("submit", send());
 
 
 function start() {
-    let magicNumber = Math.floor(Math.random() * 24) + 1;
+    let magicNumber = Math.floor(Math.random() * 25) + 1;
     console.log(magicNumber);
 }
 
+    
+// function obtenerDato(e) {
+//     e.preventDefault();
+    
+// }
 
-// function numeroAdivinado() {
-    //     }
-    
-    function obtenerDato(e) {
-        e.preventDefault();
-        console.log("ejecutando el evento del formulario");
-        let userGuess = parseInt(document.getElementById("userGuess").value);
-        console.log(userGuess);
-    
-    }
-    
-    function send() {
-    if (magicNumber === userGuess
-    ) {
-        alert(`🔮Adivinaste!🔮`);
-        document.getElementById("result").innerHTML(`🔮¡Si lo eres!🔮 <br> Tu número era ${userGuess} y el número mágico era ${magicNumber}`)
+function send() {
+    let userGuess = parseInt(document.getElementById("userGuess").value);
+    console.log(userGuess);
+    if (adivino){ 
+    alert(`${adivino}`);
     } else if (magicNumber < userGuess) {
         alert(`Tu número ${userGuess} era mayor que el número mágico ${magicNumber}`);
     } else if (magicNumber > userGuess){
         alert(`Tu número ${userGuess} era menor que el número mágico ${magicNumber}`);
-    }
+    } else if (magicNumber === userGuess) {
+    document.getElementById("result").innerHTML(`🔮¡Si lo eres!🔮 <br> Tu número era ${userGuess} y el número mágico era ${magicNumber}`);
+}
 }
